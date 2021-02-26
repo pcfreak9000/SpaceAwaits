7530f76258d5823c9246960e978c1d945503e5ae
pcfreak9000 <roman.borris@gmail.com>
2021-02-26 17:59:24 +0100

    completed the tile-fixture algorithm

--- core/src/de/pcfreak9000/spaceawaits/world/tile/ChunkPhysics.java
+++ core/src/de/pcfreak9000/spaceawaits/world/tile/ChunkPhysics.java
@@ -44,7 +44,7 @@ public class ChunkPhysics implements BodyFactory {
                 if ((top == null || !top.isSolid()) || (bot == null || !bot.isSolid())
                         || (right == null || !right.isSolid()) || (left == null || !left.isSolid())) {
                     if (t.isSolid()) {
-                        createFixture(x, y);
+                        createFixture(t, x, y);
                     }
                 }
             }
@@ -66,10 +66,12 @@ public class ChunkPhysics implements BodyFactory {
         BodyFactory.super.destroyBody(body, world, meterconv);
     }
     
-    private void createFixture(int gtx, int gty) {
+    private void createFixture(Tile tile, int gtx, int gty) { //This could be more memory efficient
         PolygonShape shape = new PolygonShape();
         FixtureDef fd = new FixtureDef();
         fd.shape = shape;
+        fd.restitution = tile.getBouncyness();
+        //Should we use the static access of METER_CONV here? Or pass it in/save it as member variable...
         shape.setAsBox(PhysicsSystemBox2D.METER_CONV.in(Tile.TILE_SIZE / 2),
                 PhysicsSystemBox2D.METER_CONV.in(Tile.TILE_SIZE / 2),
                 new Vector2(PhysicsSystemBox2D.METER_CONV.in((gtx - chunk.getGlobalTileX()) * Tile.TILE_SIZE),
@@ -89,10 +91,9 @@ public class ChunkPhysics implements BodyFactory {
                 return;
             }
             if (oldstate.getTile().isSolid() != newstate.getTile().isSolid()) {
-                if (!newstate.getTile().isSolid() && body != null) { // oldstate was solid, newstate isn't
-                    if (oldstate.getFixture() != null) {
-                        body.destroyFixture(oldstate.getFixture());
-                        oldstate.setFixture(null);
+                if (!newstate.getTile().isSolid() && body != null) { //oldstate was solid, newstate isn't
+                    if (oldstate.getFixture() != null) { //the fixture is null if oldstate was surrounded by solid tiles
+                        destroyFixture(oldstate);
                     }
                     int x = newstate.getGlobalTileX();
                     int y = newstate.getGlobalTileY();
@@ -105,16 +106,16 @@ public class ChunkPhysics implements BodyFactory {
                     TileState right = chunk.inBounds(rightx, y) ? chunk.getTileState(rightx, y) : null;
                     TileState left = chunk.inBounds(leftx, y) ? chunk.getTileState(leftx, y) : null;
                     if (top != null && top.getTile().isSolid() && top.getFixture() == null) {
-                        createFixture(x, topy);
+                        createFixture(top.getTile(), x, topy);
                     }
                     if (bot != null && bot.getTile().isSolid() && bot.getFixture() == null) {
-                        createFixture(x, boty);
+                        createFixture(bot.getTile(), x, boty);
                     }
                     if (right != null && right.getTile().isSolid() && right.getFixture() == null) {
-                        createFixture(rightx, y);
+                        createFixture(right.getTile(), rightx, y);
                     }
                     if (left != null && left.getTile().isSolid() && left.getFixture() == null) {
-                        createFixture(leftx, y);
+                        createFixture(left.getTile(), leftx, y);
                     }
                 } else { //newstate is solid, oldstate wasn't
                     int x = newstate.getGlobalTileX();
@@ -130,14 +131,37 @@ public class ChunkPhysics implements BodyFactory {
                     if ((top == null || !top.getTile().isSolid()) || (bot == null || !bot.getTile().isSolid())
                             || (right == null || !right.getTile().isSolid())
                             || (left == null || !left.getTile().isSolid())) {
-                        createFixture(x, y);
+                        createFixture(newstate.getTile(), x, y);
                     }
-                    //TODO check if the neighbouring fixtures can be removed
-                    if (top != null && top.getTile().isSolid()) {
-                        
-                    }
+                    //check if the neighbouring fixtures can be removed, this depends on the neighbours neighbours
+                    checkDestroy(top, x, topy);
+                    checkDestroy(bot, x, boty);
+                    checkDestroy(right, rightx, y);
+                    checkDestroy(left, leftx, y);
                 }
             }
+        }
+        
+        private void checkDestroy(TileState state, int tx, int ty) {
+            if (state != null && state.getTile().isSolid()) {
+                int topty = ty + 1;
+                int botty = ty - 1;
+                int righttx = tx + 1;
+                int lefttx = tx - 1;
+                Tile bot = chunk.inBounds(tx, botty) ? chunk.getTile(tx, botty) : null;
+                Tile top = chunk.inBounds(tx, topty) ? chunk.getTile(tx, topty) : null;
+                Tile right = chunk.inBounds(righttx, ty) ? chunk.getTile(righttx, ty) : null;
+                Tile left = chunk.inBounds(lefttx, ty) ? chunk.getTile(lefttx, ty) : null;
+                if ((top != null && top.isSolid()) && (right != null && right.isSolid())
+                        && (left != null && left.isSolid()) && (bot != null && bot.isSolid())) {
+                    destroyFixture(state);
+                }
+            }
+        }
+        
+        private void destroyFixture(TileState state) {
+            body.destroyFixture(state.getFixture());
+            state.setFixture(null);
         }
         
     }
