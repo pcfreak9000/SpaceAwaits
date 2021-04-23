b81145798102ff7bc7f1dffe6dc275b0e945fb2f
pcfreak9000 <roman.borris@gmail.com>
2021-04-23 21:01:41 +0200

    fixed a bug

--- core/src/de/pcfreak9000/spaceawaits/item/InvUtil.java
+++ core/src/de/pcfreak9000/spaceawaits/item/InvUtil.java
@@ -7,18 +7,19 @@ public class InvUtil {
         for (int i = 0; i < inv.slots(); i++) {
             if (inv.isItemValidForSlot(i, insert)) {
                 ItemStack cur = inv.getStack(i);
-                if (cur == null) {
+                if (cur == null || cur.isEmpty()) {
                     inv.setSlotContent(i, stack);
+                    return null;
                 } else {
                     if (ItemStack.isItemEqual(cur, stack) && ItemStack.isStackTagEqual(cur, stack)) {
                         int amount = Math.min(cur.getItem().getMaxStackSize() - cur.getCount(), stack.getCount());
                         inv.setSlotContent(i, new ItemStack(stack.getItem(), amount + cur.getCount()));
                         stack.changeNumber(-amount);
+                        if (stack.isEmpty()) {
+                            return null;
+                        }
                     }
                 }
-            }
-            if (stack.isEmpty()) {
-                return null;
             }
         }
         return stack;
